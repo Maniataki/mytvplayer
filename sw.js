1bd74dd0f413176563a33da9c171f93f24538a67
Maniataki <128607951+Maniataki@users.noreply.github.com>
2026-02-26 23:53:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'tv-app-v118';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://img.icons8.com/fluency/192/television.png',
+  'https://cdn.jsdelivr.net/npm/hls.js@latest',
+  'https://cdn.dashjs.org/latest/dash.all.min.js'
+];
+
+// Εγκατάσταση και αποθήκευση των αρχείων
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ενεργοποίηση και καθαρισμός παλιών εκδόσεων
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Διαχείριση αιτημάτων (Network falling back to cache)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
